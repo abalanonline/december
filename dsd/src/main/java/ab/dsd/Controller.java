fa396223fcf1608a91fe9acdd2ff4e52e00c64c6
Aleksei Balan <abalanonline@gmail.com>
2021-05-21 14:39:59 -0400

    test controller

---
+++ dsd/src/main/java/ab/dsd/Controller.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 Aleksei Balan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ab.dsd;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+public class Controller {
+
+  @GetMapping({"/g/{skill}", "/a/{skill}"})
+  public String test(HttpServletRequest request, @PathVariable("skill") String skill) {
+    log.info("get test " + request.getRequestURI());
+    return "get test " + skill;
+  }
+
+}
