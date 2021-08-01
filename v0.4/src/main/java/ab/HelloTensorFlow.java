54271d712d7defaccee01122f11e48b1217733fb
Aleksei Balan <abalanonline@gmail.com>
2021-07-31 22:06:44 -0400

    tf 2.4

--- v0.4/src/main/java/ab/HelloTensorFlow.java
+++ v0.4/src/main/java/ab/HelloTensorFlow.java
@@ -2,7 +2,6 @@ package ab;
 
 import org.tensorflow.ConcreteFunction;
 import org.tensorflow.Signature;
-import org.tensorflow.Tensor;
 import org.tensorflow.TensorFlow;
 import org.tensorflow.op.Ops;
 import org.tensorflow.op.core.Placeholder;
@@ -15,14 +14,14 @@ public class HelloTensorFlow {
     System.out.println("Hello TensorFlow " + TensorFlow.version());
 
     try (ConcreteFunction dbl = ConcreteFunction.create(HelloTensorFlow::dbl);
-        Tensor<TInt32> x = TInt32.scalarOf(10);
-        Tensor<TInt32> dblX = dbl.call(x).expect(TInt32.DTYPE)) {
-      System.out.println(x.data().getInt() + " doubled is " + dblX.data().getInt());
+        TInt32 x = TInt32.scalarOf(10);
+        TInt32 dblX = (TInt32)dbl.call(x)) {
+      System.out.println(x.getInt() + " doubled is " + dblX.getInt());
     }
   }
 
   private static Signature dbl(Ops tf) {
-    Placeholder<TInt32> x = tf.placeholder(TInt32.DTYPE);
+    Placeholder<TInt32> x = tf.placeholder(TInt32.class);
     Add<TInt32> dblX = tf.math.add(x, x);
     return Signature.builder().input("x", x).output("dbl", dblX).build();
   }
