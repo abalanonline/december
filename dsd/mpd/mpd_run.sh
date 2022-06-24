1288676244edc6d68908babd8b4a32a156f6ccf6
Aleksei Balan <abalanonline@gmail.com>
2022-06-24 00:36:49 -0400

    cert renew

--- dsd/mpd/mpd_run.sh
+++ dsd/mpd/mpd_run.sh
@@ -4,6 +4,7 @@ service mpd status
 cat /etc/services | flite -o /var/lib/mpd/music/audio.wav
 mpc update --wait
 mpc insert audio.wav
+mpc repeat on
 mpc play
 #read -p "Press enter to continue" ignore
 tail -f /var/log/mpd/mpd.log
