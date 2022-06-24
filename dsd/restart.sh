12587599ea0755d6e47ddf5bd5a64c38a044454b
Aleksei Balan <abalanonline@gmail.com>
2022-06-23 23:49:52 -0400

    nginx connection

--- dsd/restart.sh
+++ dsd/restart.sh
@@ -3,5 +3,5 @@ export HOSTNAME=example.com
 # docker run -d --rm --name dsmpd -p 8000:8000 dsmpd
 docker run -it --rm --name certbot -p 80:80 -v "/etc/letsencrypt:/etc/letsencrypt" \
   certbot/certbot certonly --register-unsafely-without-email --standalone -d $HOSTNAME
-docker run -d --rm --name nginx -p 443:443 -v "/etc/letsencrypt:/etc/letsencrypt" \
+docker run -d --rm --name nginx --network=host -v "/etc/letsencrypt:/etc/letsencrypt" \
   -v $PWD/nginx/:/etc/nginx/templates/ -e HOSTNAME=$HOSTNAME nginx:1.23-alpine
