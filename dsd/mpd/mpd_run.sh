2a0be3f8ddfcb593afc9199b7d360d74b2f12649
Aleksei Balan <abalanonline@gmail.com>
2022-06-25 15:22:10 -0400

    crontab

--- dsd/mpd/mpd_run.sh
+++ dsd/mpd/mpd_run.sh
@@ -4,5 +4,6 @@ mpc update --wait
 mpc insert audio.wav
 mpc repeat on
 mpc play
+echo "*/10 * * * * sh /hourly.sh" | crontab -
 #read -p "Press enter to continue" ignore
 tail -f /var/log/mpd/mpd.log
